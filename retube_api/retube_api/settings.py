cc1c9b461ccffb5e52eff51e00e65cff2ebfd4d5
Ramone Clarke <ramonejlclarke@gmail.com>
2023-04-03 15:54:13 +0100

    change access token expiry time to 15 minutes

--- retube_api/retube_api/settings.py
+++ retube_api/retube_api/settings.py
@@ -218,10 +218,10 @@ from datetime import timedelta
 ...
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=15),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
     "ROTATE_REFRESH_TOKENS": True,
-    "BLACKLIST_AFTER_ROTATION": True,
+    "BLACKLIST_AFTER_ROTATION": False,
     "UPDATE_LAST_LOGIN": False,
     "AUTH_HEADER_TYPES": ("Bearer",),
     "USER_ID_FIELD": "userId",
