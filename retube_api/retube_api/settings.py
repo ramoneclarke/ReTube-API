3662b3905bcf41df18a59f80bb21afa22382f132
Ramone Clarke <ramonejlclarke@gmail.com>
2023-04-10 15:49:47 +0100

    install whitenoise to fix static files issue

--- retube_api/retube_api/settings.py
+++ retube_api/retube_api/settings.py
@@ -83,6 +83,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.common.CommonMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = 'retube_api.urls'
