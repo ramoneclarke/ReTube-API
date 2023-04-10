22e4740b844ede1c73f860410f72013ecdb6288a
Ramone Clarke <ramonejlclarke@gmail.com>
2023-04-10 17:32:18 +0100

    change cookie domains

--- retube_api/retube_api/settings.py
+++ retube_api/retube_api/settings.py
@@ -248,11 +248,11 @@ SIMPLE_JWT = {
 CSRF_COOKIE_SECURE = False
 CSRF_COOKIE_HTTPONLY = False
 # CSRF_COOKIE_DOMAIN = ".127.0.0.1"
-CSRF_COOKIE_DOMAIN = "https://*.retube-api-production.up.railway.app"
+CSRF_COOKIE_DOMAIN = 'retube-api-production.up.railway.app'
 SESSION_COOKIE_SECURE = False
 SESSION_COOKIE_HTTPONLY = True
 # SESSION_COOKIE_DOMAIN = ".127.0.0.1"
-SESSION_COOKIE_DOMAIN = "https://*.retube-api-production.up.railway.app"
+SESSION_COOKIE_DOMAIN = 'retube-api-production.up.railway.app'
 
 # CHANGE BEFORE PUSHING TO PRODUCTION
 # CORS_ALLOW_ALL_ORIGINS = True
