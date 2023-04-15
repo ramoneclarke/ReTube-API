1ef912a2e32316c4416d163321101715495afdfb
Ramone Clarke <ramonejlclarke@gmail.com>
2023-04-15 18:26:16 +0100

    create customer portal session view and invoice paid event

--- retube_api/subscriptions/urls.py
+++ retube_api/subscriptions/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('test-payment/', views.test_payment, name='test-payment'),
     path('create-payment-intent/', views.CreatePaymentView.as_view(), name='create-payment-intent'),
     path('create-checkout-session/', views.CreateCheckoutSessionView.as_view(), name='create-checkout-session'),
+    path('create-customer-portal-session/', views.CreateCustomerPortalSessionView.as_view(), name='create-customer-portal-session'),
     path('webhooks/', views.WebhookReceivedView.as_view(), name='webhooks')
 ]
