06d00cec2496a110a7a2786df2100e69e3d7d6ca
Ramone Clarke <ramonejlclarke@gmail.com>
2023-04-18 15:57:23 +0100

    remove old social auth url path

--- retube_api/retube_api/urls.py
+++ retube_api/retube_api/urls.py
@@ -9,7 +9,6 @@ urlpatterns = [
     path('auth/', include('dj_rest_auth.urls')),
     path('auth/registration/', include('dj_rest_auth.registration.urls')),
     path('accounts/', include('allauth.urls')),
-    # path('auth/', include('drf_social_oauth2.urls', namespace='drf')),
     path("tools/", include("tools.urls")),
     path("api/", include("users.urls")),
     path("payments/", include("subscriptions.urls")),
