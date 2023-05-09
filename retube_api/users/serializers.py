3cb50f4453fb063d9cda15611bd45cfb8de27ba2
Ramone Clarke <ramonejlclarke@gmail.com>
2023-05-09 17:21:36 +0100

    add summaries to user model

--- retube_api/users/serializers.py
+++ retube_api/users/serializers.py
@@ -9,15 +9,21 @@ try:
     from allauth.account import app_settings as allauth_account_settings
     from allauth.socialaccount.helpers import complete_social_login
 except ImportError:
-    raise ImportError('allauth needs to be added to INSTALLED_APPS.')
+    raise ImportError("allauth needs to be added to INSTALLED_APPS.")
 from dj_rest_auth.registration.serializers import SocialLoginSerializer
-from tools.serializers import SnippetSerializer, YoutubePlaylistSerializer
+from tools.serializers import (
+    SnippetSerializer,
+    SummarySerializer,
+    YoutubePlaylistSerializer,
+)
 from subscriptions.serializers import SubscriptionSerializer
 from subscriptions.models import SubscriptionPlan, Subscription
 from .models import CustomUserModel
 
+
 class CustomUserModelSerializer(serializers.ModelSerializer):
     snippets = SnippetSerializer(many=True, read_only=True)
+    summaries = SummarySerializer(many=True, read_only=True)
     playlists = YoutubePlaylistSerializer(many=True, read_only=True)
     subscription = SubscriptionSerializer(read_only=True)
 
@@ -28,14 +34,16 @@ class CustomUserModelSerializer(serializers.ModelSerializer):
             "username",
             "email",
             "snippets",
+            "summaries",
             "playlists",
-            "subscription"
+            "subscription",
         ]
+
     def create(self, validated_data):
         user = CustomUserModel.objects.create_user(
             validated_data["username"],
             validated_data["email"],
-            validated_data["password"]
+            validated_data["password"],
         )
 
         return user
@@ -43,36 +51,36 @@ class CustomUserModelSerializer(serializers.ModelSerializer):
 
 class CustomSocialLoginSerializer(SocialLoginSerializer):
     def validate(self, attrs):
-        view = self.context.get('view')
+        view = self.context.get("view")
         request = self._get_request()
         if not view:
             raise serializers.ValidationError(
-                _('View is not defined, pass it as a context variable'),
+                _("View is not defined, pass it as a context variable"),
             )
-        adapter_class = getattr(view, 'adapter_class', None)
+        adapter_class = getattr(view, "adapter_class", None)
         if not adapter_class:
-            raise serializers.ValidationError(_('Define adapter_class in view'))
+            raise serializers.ValidationError(_("Define adapter_class in view"))
         adapter = adapter_class(request)
         app = adapter.get_provider().get_app(request)
         # More info on code vs access_token
         # http://stackoverflow.com/questions/8666316/facebook-oauth-2-0-code-and-token
-        access_token = attrs.get('access_token')
-        code = attrs.get('code')
+        access_token = attrs.get("access_token")
+        code = attrs.get("code")
         # Case 1: We received the access_token
         if access_token:
-            tokens_to_parse = {'access_token': access_token}
+            tokens_to_parse = {"access_token": access_token}
             token = access_token
             # For sign in with apple
-            id_token = attrs.get('id_token')
+            id_token = attrs.get("id_token")
             if id_token:
-                tokens_to_parse['id_token'] = id_token
+                tokens_to_parse["id_token"] = id_token
         # Case 2: We received the authorization code
         elif code:
             self.set_callback_url(view=view, adapter_class=adapter_class)
-            self.client_class = getattr(view, 'client_class', None)
+            self.client_class = getattr(view, "client_class", None)
             if not self.client_class:
                 raise serializers.ValidationError(
-                    _('Define client_class in view'),
+                    _("Define client_class in view"),
                 )
             provider = adapter.get_provider()
             scope = provider.get_scope(request)
@@ -92,30 +100,32 @@ class CustomSocialLoginSerializer(SocialLoginSerializer):
                 token = client.get_access_token(code)
             except OAuth2Error as ex:
                 raise serializers.ValidationError(
-                    _('Failed to exchange code for access token')
+                    _("Failed to exchange code for access token")
                 ) from ex
-            access_token = token['access_token']
-            tokens_to_parse = {'access_token': access_token}
+            access_token = token["access_token"]
+            tokens_to_parse = {"access_token": access_token}
             # If available we add additional data to the dictionary
-            for key in ['refresh_token', 'id_token', adapter.expires_in_key]:
+            for key in ["refresh_token", "id_token", adapter.expires_in_key]:
                 if key in token:
                     tokens_to_parse[key] = token[key]
         else:
             raise serializers.ValidationError(
-                _('Incorrect input. access_token or code is required.'),
+                _("Incorrect input. access_token or code is required."),
             )
         social_token = adapter.parse_token(tokens_to_parse)
         social_token.app = app
 
         try:
-            if adapter.provider_id == 'google' and not code:
+            if adapter.provider_id == "google" and not code:
                 print("Google and not code. id_token: ", id_token)
-                login = self.get_social_login(adapter, app, social_token, response={'id_token': id_token})
+                login = self.get_social_login(
+                    adapter, app, social_token, response={"id_token": id_token}
+                )
             else:
                 login = self.get_social_login(adapter, app, social_token, token)
             ret = complete_social_login(request, login)
         except HTTPError:
-            raise serializers.ValidationError(_('Incorrect value'))
+            raise serializers.ValidationError(_("Incorrect value"))
         if isinstance(ret, HttpResponseBadRequest):
             raise serializers.ValidationError(ret.content)
         if not login.is_existing:
@@ -125,19 +135,23 @@ class CustomSocialLoginSerializer(SocialLoginSerializer):
             # link up the accounts due to security constraints
             if allauth_account_settings.UNIQUE_EMAIL:
                 # Do we have an account already with this email address?
-                account_exists = get_user_model().objects.filter(
-                    email=login.user.email,
-                ).exists()
+                account_exists = (
+                    get_user_model()
+                    .objects.filter(
+                        email=login.user.email,
+                    )
+                    .exists()
+                )
                 if account_exists:
                     raise serializers.ValidationError(
-                        _('User is already registered with this e-mail address.'),
+                        _("User is already registered with this e-mail address."),
                     )
             login.lookup()
             login.save(request, connect=True)
             self.post_signup(login, attrs)
         else:
             self.post_signup(login, attrs)
-        attrs['user'] = login.account.user
+        attrs["user"] = login.account.user
         return attrs
 
     def post_signup(self, login, attrs):
@@ -151,12 +165,15 @@ class CustomSocialLoginSerializer(SocialLoginSerializer):
         # Create a new subscription object for the user
         try:
             plan = SubscriptionPlan.objects.get(name="free")
-            subscription, created = Subscription.objects.get_or_create(user=login.user, defaults={
-                'plan': plan,
-                'snippets_usage': 0,
-                'summaries_usage': 0,
-                'search_playlists_active': 0
-            })
+            subscription, created = Subscription.objects.get_or_create(
+                user=login.user,
+                defaults={
+                    "plan": plan,
+                    "snippets_usage": 0,
+                    "summaries_usage": 0,
+                    "search_playlists_active": 0,
+                },
+            )
             if created:
                 print("Subscription object created for user:", login.user)
             else:
